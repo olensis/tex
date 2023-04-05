c3d64834c6b9a7c6ac596826e32e744952ed01ff
olensis <olensis7gmail@.com>
2023-04-05 11:56:29 +0300

    2

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -4,31 +4,5 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class User {
-    String name;
-    int age;
 
-    public User (String name, int a) {
-        this.name = name;
-        this.age = a;
-    }
-
-    public String Name() {
-        return name;
-    }
-
-    public void NewName(String name) {
-        this.name = name;
-    }
-
-    public int value() {
-        return age;
-    }
-
-    public void NewAge(int age) {
-        this.age = age;
-    }
-
-    public String toString() {
-        return "Name: " + this.name + "-- age: " + this.age;
-    }
 }
