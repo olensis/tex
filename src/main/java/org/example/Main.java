8e7c73aaf1a1eb51df9dbe876c0977708646e35d
olensis <olensis7gmail@.com>
2023-04-05 11:54:17 +0300

    1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeSet<User> users = new TreeSet<>();
+
+        // выбрала TreeSet, потому что там не может быть дублей и ThreeSet сортирует
+
+        users.add(new User("Kat", 20));
+        users.add(new User("Man", 22));
+    }
+}
